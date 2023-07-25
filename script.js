c97c54e08ba1f658a5b16260bb697f250839a43b
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2023-07-26 01:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function highlight() {
     //Write your code here
-
+	let boldText = document.querySelectorAll("strong")
+	boldText.forEach((element) => {
+		element.style.color = "rgb(0, 128, 0)"
+			// "green"
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let boldText = document.querySelectorAll("strong")
+	boldText.forEach((element) => {
+		element.style.color = "rgb(0, 0, 0)"
+			// "black"
+	})
     
 }
+let link = document.querySelector("a")
+// let link = document.getElementByTagName("a")[0]
+link.addEventListner('onMouseOver', highlight)
+link.addEventListner('onMouseOut', return_normal)
